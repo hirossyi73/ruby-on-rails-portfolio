b05cb29f39df0a3fa3259d7b94cb0509edc3d5c5
hirossyi73 <it2@hirossyi.net>
2025-09-28 21:21:43 +0900

    Todo作成機能の追加とルーティングの更新

--- src/app/controllers/todos_controller.rb
+++ src/app/controllers/todos_controller.rb
@@ -2,4 +2,24 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.order(created_at: :desc)
   end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to root_path, notice: "Todoを作成しました。"
+    else
+      flash.now[:alert] = "Todoの作成に失敗しました。"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :completed)
+  end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   # Add a simple Todos index as the root page
-  resources :todos, only: [:index]
+  resources :todos, only: [:index, :new, :create]
   root "todos#index"
 end
