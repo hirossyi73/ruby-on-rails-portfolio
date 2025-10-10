229aecc8462c8351cda10874eef4ce49c113211a
hirossyi73 <it2@hirossyi.net>
2025-10-10 16:42:58 +0900

    フォルダ構成修正、webフォルダを削除

--- web/src/app/javascript/application.js
+++
@@ -1,8 +0,0 @@
-// Entrypoint for importmap-managed JavaScript
-import '@hotwired/turbo-rails'
-import Rails from '@rails/ujs'
-
-Rails.start()
-
-// Additional JS imports can go here, e.g. Stimulus controllers
-// import "controllers"
--- web/src/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  
-  validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
-  
-  # 関連付け
-  has_many :todos, dependent: :destroy
-end
